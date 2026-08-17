ed40f2b77e838bcb3d67e5a82d3876bdd1c79117
Jeff Margolies <jeffmargolies@gmail.com>
2026-08-16 19:38:39 -0700

    update message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Hello World</h1><p>${new Date().toString()}</p>`);
+  res.send(`<h1>Hello Jeff</h1><p>${new Date().toString()}</p>`);
 });
 
 app.listen(port, () => {
